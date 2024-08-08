f2e3ec1eb48adbf0ba96e6bd2fe5eca5d61624d9
Mister-Bajaan <pvpaaseydoubadiane@gmail.com>
2024-08-08 18:27:38 +0000

    Configure color palette for the design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,40 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+
+      blue: {
+        300:"#4256D0",
+        400: "#5C73DB",
+        500: "#4763E4",
+        700:"#5145CD",
+        800:"#0E1B6B"
+      },
+
+      green: {
+        100: "#DCFCE7",
+        200: "#03543F",
+      },
+
+      red: {
+        100: "##FEE2E2",
+        600: "#DC2626",
+        800: "##991B1B",
+      },
+
+      gray: {
+        100:"#F4F4F5",
+        200: "#E4E4E7",
+        400: "#A1A1AA",
+        500: "#71717A",
+        800: "#27272A",
       },
     },
+
+    extend: {},
   },
+
   plugins: [],
 };

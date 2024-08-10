706c444b712b13c3c6ad77a8556d552e06e68382
Mister-Bajaan <pvpaaseydoubadiane@gmail.com>
2024-08-10 19:19:41 +0000

    test pour le deploiement sur vercel

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="w-full bg-gray-100 space-y-4">
+    <div className="flex ">
+      <div className="w-full bg-gray-100 space-y-4 flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold">
           Page inexitant, utiliser l'url /dashboard
         </h1>
